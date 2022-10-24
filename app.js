4a184320b263c885a74e925a723682712bc89cba
Ashwin Jose <87763145+ashwinjose@users.noreply.github.com>
2022-10-24 16:37:39 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+setInterval(setClock, 1000);
+
+const secondHand = document.querySelector("[data-second-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const hourHand = document.querySelector("[data-hour-hand]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+setClock();
